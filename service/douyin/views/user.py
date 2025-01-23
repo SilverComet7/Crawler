030778c996e055ddf9eb02ebaf53cb7d32086624
ChrisWang <841531691@qq.com>
2025-01-23 09:08:14 +0800

    FEAT

--- service/douyin/views/user.py
+++ service/douyin/views/user.py
@@ -6,7 +6,7 @@ from ..logic import request_user
 import random
 
 # route
-async def user(id: str, offset: int = 0, limit: int = 10):
+async def user(id: str, offset: int = 0, limit: int = 10 , accountName: str = 'Feeling'):
     """
     获取用户信息
     """
@@ -16,6 +16,10 @@ async def user(id: str, offset: int = 0, limit: int = 10):
         if account.get('expired', 0) == 1:
             continue
         account_id = account.get('id', '')
+        # print(account_id,account)
+        # 优先 accountName 指定查询避免随机
+        if accountName != account_id:
+            continue
         res = await request_user(id, account.get('cookie', ''), offset, limit)
         logger.info(f'get user detail success, account: {account_id}, id: {id}, res: {res}')
         return reply(ErrorCode.OK, '成功' , res)
